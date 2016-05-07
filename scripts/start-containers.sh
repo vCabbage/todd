3eae70be7ba961a6f297d0d48adfb4bdb88d87b6
Kale Blankenship <kale@lemnisys.com>
2016-05-07 13:28:55 -0700

    Simplify getting started with ToDD
    * Makes ToDD "go gettable. Running `go get github.com/Mierdin/todd/...`
      pulls code and builds binaries into `$GOPATH/bin`.
    * Removed (what I believe to be) no longer necessary build steps in Makefile.
    * Check-in assets. Assets being part of the code it is typical to check
      assets into git. Required to make todd "go gettable". Added `make update_assets` command.
    * Added `make start` command to start a simple ToDD environment in docker.
    * Modified Vagrantfile to mount code directly into GOPATH to simplify
      dev. Code can be changed on host and rebuilt in Vagrant with
      `go install ./...`, `make install`, or `make start`.
    * Moved main packages into `cmd/[cmd name]`. This is a common structure
      and results in `go get ./...` installing binaries with the correct names.
    * Updated `server/assets.go` to serve files from assets package directly
      rather than extracting to filesystem.
    * Modified `etc/{agent,server}.cfg` to use localhost rather than
      hardcoded hosts.
    * Migrated godeps specific layout to vendor, which is supported by
      default in go 1.6. This eliminates the need to install godep to
      build ToDD. godep does need to be installed to manage vendoring.
    * `scripts/ansible-todd-dev.yml` updates:
      * Changed the deprecated `sudo` to `become`/`become_user`.
      * Updated gover to 1.6.2.
      * Added gosha256 var to verify go download.
      * Linked `etc/` to `/etc/todd` so that changes on the host are
        available in vagrant VM.
      * Modified paths and permissions so that sudo is not required to build
        or run ToDD.
      * Added `scripts/` to PATH.
      * Modified `.profile` so that `vagrant ssh` will take users directly
        to `$GOPATH/src/github.com/Mierdin/todd`.
      * Updated `scripts/start-containers.sh` to allow specifying number of
        agents and config files as arguments. Added extra logic to prevent
        error messages when containers don't exist or when on Linux and not
        using docker-machine.

--- scripts/start-containers.sh
+++ scripts/start-containers.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright 2016 Matt Oswalt. Use or modification of this
 # source code is governed by the license provided here:
 # https://github.com/mierdin/todd:$branch/blob/master/LICENSE
@@ -13,22 +15,34 @@ alias dtodd='docker run --rm --net todd-network --name="todd-client" mierdin/tod
 
 # Clean up old containers
 function cleanup {
-    docker kill todd-server
-    docker kill $(docker ps -aq --filter "label=toddtype=agent")
-    docker kill rabbit
-    docker kill etcd
-    docker kill influx
-    docker kill grafana
-    docker rm rabbit
-    docker rm etcd
-    docker rm influx
-    docker rm grafana
-    docker rm todd-server
-    docker rm $(docker ps -aq --filter "label=toddtype=agent")
+    echo "Removing existing containers"
+    for name in todd-server rabbit etcd influx grafana $(docker ps -aq --filter "label=toddtype=agent")
+    do
+        container=$(docker ps -q -f name="${name}" | wc -l)
+        if [ $container -gt 0 ]; then
+            docker kill "${name}" > /dev/null
+        fi
+        
+        container=$(docker ps -aq -f name="${name}" | wc -l)
+        if [ $container -gt 0 ]; then
+            docker rm "${name}" > /dev/null
+        fi
+    done
+
+    for id in $(docker ps -aq --filter "label=toddtype=agent")
+    do
+        container=$(docker ps -q -f id="${id}" | wc -l)
+        if [ $container -gt 0 ]; then
+            docker kill "${id}" > /dev/null
+        fi
+        
+        docker rm "${id}" > /dev/null
+    done
 }
 
 # Run infra containers
 function startinfra {
+    echo "Starting etcd"
     docker run -d --net todd-network -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
      --name etcd quay.io/coreos/etcd:v2.0.8 \
      -name etcd0 \
@@ -38,9 +52,10 @@ function startinfra {
      -listen-peer-urls http://0.0.0.0:2380 \
      -initial-cluster-token etcd-cluster-1 \
      -initial-cluster etcd0=http://${HostIP}:2380 \
-     -initial-cluster-state new
+     -initial-cluster-state new > /dev/null
 
     # I have a rabbitmq server at home, but in case I'm working on my laptop only, spin this up too:
+    echo "Starting RabbitMQ"
     docker run -d \
         --net todd-network \
         --name rabbit \
@@ -48,10 +63,12 @@ function startinfra {
         -p 5672:5672 \
         -e RABBITMQ_DEFAULT_USER=guest \
         -e RABBITMQ_DEFAULT_PASS=guest \
-        rabbitmq:3-management
+        rabbitmq:3-management > /dev/null
 
-    docker run -d --net todd-network --volume=/var/influxdb:/data --name influx -p 8083:8083 -p 8086:8086 tutum/influxdb:0.9
-    docker run -d --net todd-network --volume=/var/lib/grafana:/var/lib/grafana --name grafana -p 3000:3000 grafana/grafana
+    echo "Starting InfluxDB"
+    docker run -d --net todd-network --volume=/var/influxdb:/data --name influx -p 8083:8083 -p 8086:8086 tutum/influxdb:0.9 > /dev/null
+    echo "Starting Grafana"
+    docker run -d --net todd-network --volume=/var/lib/grafana:/var/lib/grafana --name grafana -p 3000:3000 grafana/grafana > /dev/null
 
 }
 
@@ -59,12 +76,14 @@ function startinfra {
 # arg $2: server config location
 # arg $3: agent config location
 function starttodd {
-    docker run -d -h="todd-server" -p 8081:8081 -p 8080:8080 -p 8090:8090 --net todd-network --name="todd-server" mierdin/todd:$branch todd-server --config="$2"
+    echo "Starting todd-server"
+    docker run -d -h="todd-server" -p 8081:8081 -p 8080:8080 -p 8090:8090 --net todd-network --name="todd-server" mierdin/todd:$branch todd-server --config="$2" > /dev/null
 
     i="0"
     while [ $i -lt $1 ]
     do
-        docker run -d --label toddtype="agent" -h="todd-agent-$i" --net todd-network --name="todd-agent-$i" mierdin/todd:$branch todd-agent --config="$3"
+        echo "Starting todd-agent-${i}"
+        docker run -d --label toddtype="agent" -h="todd-agent-$i" --net todd-network --name="todd-agent-$i" mierdin/todd:$branch todd-agent --config="$3" > /dev/null
         i=$[$i+1]
     done
 
@@ -102,18 +121,27 @@ function runintegrationtests {
 
 }
 
-docker-machine start docker-dev
-eval $(docker-machine env docker-dev)
+if hash docker-machaine 2> /dev/null
+then
+    echo "Starting docker machine"
+    docker-machine start docker-dev
+    eval $(docker-machine env docker-dev)
 
-docker pull mierdin/todd:$branch
-
-export HostIP=$(docker-machine ip docker-dev)
-# Set HostIP to localhost if docker-machine doesn't run
-if [ $HostIP="" ]; then
-    export HostIP="127.0.0.1"
+    export HostIP=$(docker-machine ip docker-dev)
+    # Set HostIP to localhost if docker-machine doesn't run
+    if [ $HostIP="" ]; then
+        export HostIP="127.0.0.1"
+    fi
 fi
 
-docker network create todd-network
+image=mierdin/todd:$branch
+echo "Pulling image ${image}"
+docker pull $image > /dev/null
+
+if [ $(docker network ls | grep todd-network | wc -l) -lt 1 ]; then
+    echo "Creating todd-network"
+    docker network create todd-network
+fi
 
 cleanup
 
@@ -128,6 +156,7 @@ then
     itsetup
 
     runintegrationtests
+    exit $?
 fi
 
 if [ "$1" == "interop" ]
@@ -135,5 +164,12 @@ then
     sleep 5
 
     starttodd 3 /etc/todd/server-interop.cfg /etc/todd/agent-interop.cfg
+    exit $?
+fi
 
+if [ -n "$1" ]
+then
+    sleep 5
+    
+    starttodd "$1" "$2" "$3"
 fi
