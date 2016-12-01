9650d02b00cfd3a1d714a5e19ebed1d27aecbe4d
Kale Blankenship <kale@lemnisys.com>
2016-11-30 16:02:40 -0800

    Refactor server/grouping

--- server/grouping/grouping.go
+++ server/grouping/grouping.go
@@ -13,6 +13,7 @@ import (
 	"regexp"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/pkg/errors"
 
 	"github.com/toddproject/todd/agent/defs"
 	"github.com/toddproject/todd/agent/tasks"
@@ -24,51 +25,52 @@ import (
 
 // CalculateGroups is a function designed to ingest a list of group objects, a collection of agents, and
 // return a map that contains the resulting group for each agent UUID.
-func CalculateGroups(cfg *config.Config, tdb db.Database) {
+func CalculateGroups(cfg *config.Config, tdb db.Database, tc comms.Comms) error { // TODO: Currently an agent can only be in a single group, allow multiple
 	// Retrieve all currently active agents
 	agents, err := tdb.GetAgents()
 	if err != nil {
-		log.Fatalf("Error retrieving agents: %v", err)
+		return errors.Wrap(err, "retrieving agents")
 	}
 
 	// Retrieve all objects with type "group"
-	groupObs, err := tdb.GetObjects("group")
+	groupObjs, err := tdb.GetObjects("group")
 	if err != nil {
-		log.Fatalf("Error retrieving groups: %v", err)
+		return errors.Wrap(err, "retrieving groups")
 	}
 
 	// Cast retrieved slice of ToddObject interfaces to actual GroupObjects
-	groups := make([]objects.GroupObject, len(groupObs))
-	for i, gobj := range groupObs {
+	groups := make([]objects.GroupObject, len(groupObjs))
+	for i, gobj := range groupObjs {
 		groups[i] = gobj.(objects.GroupObject)
 	}
 
 	// groupmap contains the uuid-to-groupname mappings to be used for test runs
-	groupmap := map[string]string{}
+	groupmap := make(map[string]string)
 
 	// This slice will hold all of the agents that are sad because they didn't get into a group
 	var lonelyAgents []defs.AgentAdvert
 
 next:
-	for x := range agents {
-
-		for i := range groups {
-
+	for _, agent := range agents {
+		for _, group := range groups {
 			// See if this agent is in this group
-			if isInGroup(groups[i].Spec.Matches, agents[x].Facts) {
-
-				// Insert this group name ("Label") into groupmap under the key of the UUID for the agent that belongs to it
-				log.Debugf("Agent %s is in group %s\n", agents[x].UUID, groups[i].Label)
-				groupmap[agents[x].UUID] = groups[i].Label
-				continue next
-
+			ok, err := isInGroup(group.Spec.Matches, agent.Facts)
+			if err != nil {
+				return errors.Wrapf(err, "checking group for %q", agent.UUID)
 			}
+			if !ok {
+				continue
+			}
+
+			// Insert this group name ("Label") into groupmap under the key of the UUID for the agent that belongs to it
+			log.Debugf("Agent %s is in group %s", agent.UUID, group.Label)
+			groupmap[agent.UUID] = group.Label
+			continue next
 		}
 
 		// The "continue next" should prohibit all agents that have a group from getting to this point,
 		// so the only ones left do not have a group.
-		lonelyAgents = append(lonelyAgents, agents[x])
-
+		lonelyAgents = append(lonelyAgents, agent)
 	}
 
 	// Write results to database
@@ -78,29 +80,70 @@ next:
 	}
 
 	// Send notifications to each agent to let them know what group they're in, so they can cache it
-	tc, err := comms.New(cfg)
-	if err != nil {
-		log.Fatalf("Error setting up ToDD Comms during group calculation")
-	}
-
 	for uuid, groupName := range groupmap {
 		setGroupTask := &tasks.SetGroup{
 			BaseTask:  tasks.BaseTask{Type: "SetGroup"},
 			GroupName: groupName,
 		}
-
 		tc.SendTask(uuid, setGroupTask)
 	}
 
 	// need to send a message to all agents that weren't in groupmap to set their group to nothing
-	for x := range lonelyAgents {
+	for _, agent := range lonelyAgents {
 		setGroupTask := &tasks.SetGroup{
 			BaseTask:  tasks.BaseTask{Type: "SetGroup"},
 			GroupName: "",
 		}
-
-		tc.SendTask(lonelyAgents[x].UUID, setGroupTask)
+		tc.SendTask(agent.UUID, setGroupTask)
 	}
+
+	return nil
+}
+
+type matcher func(value string, facts map[string][]string) (bool, error)
+
+var matchers = map[string]matcher{
+	"hostname": func(regex string, facts map[string][]string) (bool, error) {
+		// Continue if no "Hostname" fact present
+		hostnames, ok := facts["Hostname"]
+		if !ok {
+			return false, nil
+		}
+
+		exp, err := regexp.Compile(regex)
+		if err != nil {
+			return false, errors.Wrapf(err, "parsing %s", regex)
+		}
+
+		for _, hostname := range hostnames {
+			if exp.MatchString(hostname) {
+				return true, nil
+			}
+		}
+
+		return false, nil
+	},
+	"within_subnet": func(subnet string, facts map[string][]string) (bool, error) {
+		// Continue if no "Addresses" fact present
+		addresses, ok := facts["Addresses"]
+		if !ok {
+			return false, nil
+		}
+
+		// First, we retrieve the desired subnet from the grouping object, and convert to net.IPNet
+		_, desiredNet, err := net.ParseCIDR(subnet)
+		if err != nil {
+			return false, errors.Wrapf(err, "parsing %s", subnet)
+		}
+
+		for _, address := range addresses {
+			if desiredNet.Contains(net.ParseIP(address)) {
+				return true, nil
+			}
+		}
+
+		return false, nil
+	},
 }
 
 // isInGroup takes a set of match statements (typically present in a group object definition) and a map of a single agent's facts,
@@ -113,63 +156,24 @@ next:
 // - within_subnet
 //
 // In the future, this functionality will be made much more generic, in order to take advantage of any user-defined fact collectors.
-func isInGroup(matchStatements []map[string]string, factmap map[string][]string) bool {
+func isInGroup(matchStatements []map[string]string, facts map[string][]string) (bool, error) {
+	match := false
 
 	// Iterate over the match statements
-	for x := range matchStatements {
-
-		// If a "hostname" statement is present, perform a check
-		if _, ok := matchStatements[x]["hostname"]; ok {
-
-			// Continue if no "Hostname" fact present
-			if _, ok := factmap["Hostname"]; !ok {
-				continue
+	for _, matchStatement := range matchStatements {
+		for name, value := range matchStatement {
+			matcher, ok := matchers[name]
+			if !ok {
+				return false, errors.Errorf("unknown matcher: %q", name)
 			}
 
-			exp, err := regexp.Compile(matchStatements[x]["hostname"])
+			ok, err := matcher(value, facts)
 			if err != nil {
-				log.Warn("Unable to compile provided regular expression in group object")
-				continue
+				return false, err
 			}
-
-			regexStrs := factmap["Hostname"]
-			for j := range matchStatements {
-				result := exp.Find([]byte(regexStrs[j]))
-				if result != nil {
-					return true
-				}
-			}
-
+			match = match || ok
 		}
-
-		// If a "within_subnet" statement is present, perform a check
-		if _, ok := matchStatements[x]["within_subnet"]; ok {
-
-			// Continue if no "Addresses" fact present
-			if _, ok := factmap["Addresses"]; !ok {
-				continue
-			}
-
-			thisSubnet := matchStatements[x]["within_subnet"]
-
-			// First, we retrieve the desired subnet from the grouping object, and convert to net.IPNet
-			_, desiredNet, err := net.ParseCIDR(thisSubnet)
-			if err != nil {
-				log.Errorf("Problem parsing desired network in grouping object: %q", thisSubnet)
-			}
-
-			addresses := factmap["Addresses"]
-
-			for y := range addresses {
-
-				if desiredNet.Contains(net.ParseIP(addresses[y])) {
-					return true
-				}
-			}
-
-		}
-
 	}
 
-	return false
+	return match, nil
 }
