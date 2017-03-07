3978547785a54201a4878c8bb5284b1da2759bb9
Matt Oswalt <matt@keepingitclassless.net>
2017-03-06 23:02:54 -0800

    Removed duplicate 'go get' in lieu of vendored linters
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,5 @@
 # Install dependencies
-go get -u github.com/golang/lint/golint
 go get -u github.com/alecthomas/gometalinter
 
 # Lint checks, skipping vendor and assets directory
-gometalinter --disable-all --enable=golint --skip=assets --vendor ./...
+gometalinter --install --disable-all --enable=golint --skip=assets --vendor ./...
